de715ea179e5efa17a6bb6c3ec07e377287cb8cf
Sun English <sunenglishjr@gmail.com>
2025-01-10 21:24:05 -0500

    circle added

--- src/map.jsx
+++ src/map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const ResourceMap = () => {
@@ -9,6 +9,12 @@ const ResourceMap = () => {
     { id: 2, position: [37.55012, -77.44475], name: "Marker 2", description: "This is the second marker" },
   ];
 
+  const center = [37.54812, -77.44675]
+  const fillBlueOptions = { fillColor: 'blue' }
+const fillRedOptions = { fillColor: 'red' }
+const greenOptions = { color: 'green', fillColor: 'green' }
+const purpleOptions = { color: 'purple' } 
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <MapContainer center={[37.54812, -77.44675]} zoom={13} style={{ height: "100%", width: "100%" }}>
@@ -17,6 +23,7 @@ const ResourceMap = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
+              <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
 
         {/* Add markers */}
         {markers.map((marker) => (
